bbe8163a91a3b3abedc21678241cd6dc684f6ad1
Vansh-Patel1718 <patelvarun1205@gmail.com>
2026-01-16 01:17:14 +0530

    home_screen

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
 
             )
         }
-        <button className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'>Get started <ArrowRight className='w-4 h-4'/></button>
+        {/* <button className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'>Get started <ArrowRight className='w-4 h-4'/></button> */}
     </div>
   )
 }
