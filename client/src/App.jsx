5f2cd5414fa5d934855a5a8a3b2ea87a60e01db7
Vansh-Patel1718 <patelvarun1205@gmail.com>
2026-01-15 18:12:48 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+
+const App = () => {
+  return  (
+    <div>
+    <Routes>
+      <Route path ='/' element={<Home />} />
+      <Route path ='/ai' element={<Layout />} />
+    </Routes>
+    </div>
+  )
+}
+
+export default App
