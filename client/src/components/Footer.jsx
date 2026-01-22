0c64d5263a94c5f19994cb79c66d146c46e82669
Vansh-Patel1718 <patelvarun1205@gmail.com>
2026-01-22 19:49:08 +0530

    Dashboard

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,11 +3,11 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
     return (
-        <div className='text-gray-500/80 pt-8 px-6 md:px-16 lg:px-24 xl:px-32'>
-            <div className='flex flex-wrap justify-between gap-12 md:gap-6'>
-                <div className='max-w-80'>
-                    <img src={assets.logo} alt="logo" className='mb-4 h-8 md:h-9' />
-                    <p className='text-sm'>
+        <footer className='px-6 md:px-16 lg:px-24 xl:px-32 pt-8 w-full text-gray-500 mt-20'>
+            <div className='flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500/30 pb-6'>
+                <div className='md:max-w-96'>
+                    <img src={assets.logo} alt="logo" className='   h-9' />
+                    <p className='mt-6 text-sm'>
                         Experience the power of AI with CognixAI.<br />Transform your content creation with our suite of premium AI tools. Write articles, generate images, and optimize SEO effortlessly.
                     </p>
                     <div className='flex items-center gap-3 mt-4'>
@@ -59,7 +59,7 @@ const Footer = () => {
                     </p>
                     <div className='flex items-center mt-4'>
                         <input type="text" className='bg-white rounded-l border border-gray-300 h-9 px-3 outline-none' placeholder='Your email' />
-                        <button className='flex items-center justify-center bg-black h-9 w-9 aspect-square rounded-r'>
+                        <button className='flex items-center justify-center bg-black h-9 w-9 aspect-square rounded-r cursor-pointer'>
                             {/* Arrow icon */}
                             <svg className="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 12H5m14 0-4 4m4-4-4-4" /></svg>
                         </button>
@@ -68,14 +68,14 @@ const Footer = () => {
             </div>
             <hr className='border-gray-300 mt-8' />
             <div className='flex flex-col md:flex-row gap-2 items-center justify-between py-5'>
-                <p>© {new Date().getFullYear()} <a href="https://prebuiltui.com">PrebuiltUI</a>. All rights reserved.</p>
+                <p className='text-center'>© {new Date().getFullYear()} <a href="https://prebuiltui.com">PrebuiltUI</a>. All rights reserved.</p>
                 <ul className='flex items-center gap-4'>
                     <li><a href="#">Privacy</a></li>
                     <li><a href="#">Terms</a></li>
                     <li><a href="#">Sitemap</a></li>
                 </ul>
             </div>
-        </div>
+        </footer>
     );
 };
 
