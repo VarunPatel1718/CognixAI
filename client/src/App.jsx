889981c6697baf4ea2a5630ee55479f4b74509f3
Vansh-Patel1718 <patelvarun1205@gmail.com>
2026-01-24 00:45:38 +0530

    frontend_completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,24 +10,32 @@ import RemoveBackground from './pages/RemoveBackground'
 import RemoveObject from './pages/RemoveObject'
 import ReviewResume from './pages/ReviewResume'
 import Community from './pages/Community'
+import { useAuth } from '@clerk/clerk-react'
+import { useEffect } from 'react'
 
 
 const App = () => {
-  return  (
+  const { getToken } = useAuth();
+
+  useEffect(() => {
+    getToken().then((token) => console.log(token));
+  }, []);
+
+  return (
     <div>
-    <Routes>
-      <Route path ='/' element={<Home />} />
-      <Route path ='/ai' element={<Layout />}>
-        <Route index element={<Dashboard/>} />
-        <Route path='write-article' element={<WriteArticle/>} />
-        <Route path='blog-titles' element={<BlogTitles/>} />
-        <Route path='generate-images' element={<GenerateImages/>} />
-        <Route path='remove-background' element={<RemoveBackground/>} />
-        <Route path='remove-object' element={<RemoveObject/>} />
-        <Route path='review-resume' element={<ReviewResume/>} />
-        <Route path='community' element={<Community/>} />
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/ai' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path='write-article' element={<WriteArticle />} />
+          <Route path='blog-titles' element={<BlogTitles />} />
+          <Route path='generate-images' element={<GenerateImages />} />
+          <Route path='remove-background' element={<RemoveBackground />} />
+          <Route path='remove-object' element={<RemoveObject />} />
+          <Route path='review-resume' element={<ReviewResume />} />
+          <Route path='community' element={<Community />} />
+        </Route>
+      </Routes>
     </div>
   )
 }
